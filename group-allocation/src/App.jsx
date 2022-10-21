852b83c7eb57a5701668f7c195ecb59c7c08f1d2
ESIEA-Dive <dive@et.esiea.fr>
2022-10-21 12:15:00 +0200

    Structure

---
+++ group-allocation/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+import { Content, Footer, Header } from './container/index';
+
+function App() {
+
+  return (
+  
+  <div className='app__flex'>
+    <Header />
+    <Content />
+    <Footer />
+  </div>
+  
+  )
+};
+
+export default App;
